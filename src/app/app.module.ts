f415df738ebe81b20dae3493ef759c170d10169a
Manideep <manideep.digischool@gmail.com>
2022-06-14 08:52:54 +0530

    directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  banners: Array<any> = [
+      {
+          id: 1,
+          imgSrc: 'https://img.freepik.com/free-vector/e-commerce-icon-robotic-hand-internet-shopping-online-purchase-add-cart_127544-586.jpg?w=2000',
+          alternateText: 'Banner 1'
+      },
+      {
+          id: 2,
+          imgSrc: 'https://img.freepik.com/free-photo/omni-channel-technology-online-retail-business_31965-3010.jpg?w=2000',
+          alternateText: 'Banner 2'
+      },
+      {
+          id: 3,
+          imgSrc: '',
+          alternateText: 'Banner 3'
+      }
+  ];
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.banners[1].imgSrc)
+  }
+  getBannerInfo(bannerInfo: any) {
+    console.log(bannerInfo)
   }
 
 }
